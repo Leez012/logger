81d72c9840d973b8556f4ead32171571da3c3c95
Semin Kwon <leez012>
2018-09-11 13:06:27 +0900

    bug fix : directory check error

--- logger.go
+++ logger.go
@@ -24,6 +24,9 @@ type Logger struct {
 // InitStandardLogger : 표준 로거 설정
 func (l *Logger) InitStandardLogger(logType uint) {
 	if logType == 0 {
+		if l.Directory == "" {
+			l.Directory = "logs"
+		}
 		if _, err := os.Stat(l.Directory); os.IsNotExist(err) {
 			err = os.MkdirAll(l.Directory, 0755)
 			if err != nil {
@@ -48,9 +51,7 @@ func (l *Logger) logLotate() {
 		timeDate := time.Now()
 		l.Date = &timeDate
 	}
-	if l.Directory == "" {
-		l.Directory = "logs"
-	}
+
 	if l.fileName == "" || *l.Date != time.Now() {
 		l.fileName = fmt.Sprintf("%s/%s-%s.log", l.Directory, l.Prefix, l.Date.Format("2006-01-02"))
 		l.logFD, err = os.OpenFile(l.fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
